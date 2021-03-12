98b1ee0789ff1abf5601dfa1d39cc787190abdbe
ShoumoPal <46050414+ShoumoPal@users.noreply.github.com>
2021-03-12 12:07:03 +0530

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10f;
+    private float bound = 2.55f;
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 currentPosition = transform.position;
+
+        //going to the right side
+        if (h > 0)
+        {
+            currentPosition.x += speed * Time.deltaTime;
+        }
+        if (h < 0)
+        {
+            currentPosition.x -= speed * Time.deltaTime;
+        }
+
+        if(currentPosition.x>bound)
+        {
+            currentPosition.x = bound;
+        }
+        else if(currentPosition.x<(-bound))
+        {
+            currentPosition.x = -bound;
+        }
+        transform.position = currentPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag=="Bomb")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    private float bound = 2.55f;
+    public GameObject bombPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnBombs());
+    }
+
+    IEnumerator SpawnBombs()
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0f, 1f));
+        Instantiate(bombPrefab, new Vector2(UnityEngine.Random.Range(-bound, bound), transform.position.y), Quaternion.identity);
+        StartCoroutine(SpawnBombs());
+    }
+
+    void Update()
+    {
+        
+    }
+}
